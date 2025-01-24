f7b43faab72ce6cf56a98f70a78fcbc12c00c087
Niclas Lietzow <niclas.lietzow@outlook.com>
2025-01-24 15:49:19 +0100

    refine register

--- hockey/register.py
+++ hockey/register.py
@@ -1,24 +1,24 @@
 import logging
 from gymnasium.envs.registration import register
 
-REGISTERED_ENVS = False
+register(
+  id="Hockey-v0",
+  entry_point="hockey.hockey_env:HockeyEnv",
+  kwargs={"mode": 0},
+)
+register(
+  id="Hockey-One-SB3-v0",
+  entry_point="hockey.hockey_env:HockeyEnvSB3Opponent",
+  kwargs={"mode": 0},
+)
+register(
+  id="Hockey-One-v0",
+  entry_point="hockey.hockey_env:HockeyEnv_BasicOpponent",
+  kwargs={"mode": 0, "weak_opponent": False},
+)
 
-try:
-    register(
-        id="Hockey-v0",
-        entry_point="hockey.hockey_env:HockeyEnv",
-        kwargs={"mode": 0},
-    )
-    register(
-        id="Hockey-One-v0",
-        entry_point="hockey.hockey_env:HockeyEnv_BasicOpponent",
-        kwargs={"mode": 0, "weak_opponent": False},
-    )
-    register(
-        id="Hockey-One-SB3-v0",
-        entry_point="hockey.hockey_env:HockeyEnvSB3Opponent",
-        kwargs={"mode": 0, "weak_opponent": False},
-    )
-    REGISTERED_ENVS = True
-except Exception as e:
-    logging.error("Error registering environments in hockey:", e)
+REGISTERED_ENVS = [
+  "Hockey-v0",
+  "Hockey-One-SB3-v0",
+  "Hockey-One-v0",
+]
