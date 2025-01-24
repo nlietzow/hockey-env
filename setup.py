4c66cb935e29199a6fe4c1dc8059cabb45be0bcd
Niclas Lietzow <niclas.lietzow@outlook.com>
2025-01-24 15:22:59 +0100

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="hockey",
-    version="2.10",
+    version="2.11",
     description="Simple Hockey Environments",
     url="https://github.com/martius-lab",
     author="Georg Martius, Uni Tuebingen, Autonomous Learning",
