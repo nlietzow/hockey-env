4bfe7e719bbc10b74f129f87b0d99c7ae0d8b808
Niclas Lietzow <niclas.lietzow@outlook.com>
2025-01-24 03:50:41 +0100

    formatting

--- hockey/register.py
+++ hockey/register.py
@@ -4,16 +4,16 @@ from gymnasium.envs.registration import register
 REGISTERED_ENVS = False
 
 try:
-  register(
-    id="Hockey-v0",
-    entry_point="hockey.hockey_env:HockeyEnv",
-    kwargs={"mode": 0},
-  )
-  register(
-    id="Hockey-One-v0",
-    entry_point="hockey.hockey_env:HockeyEnv_BasicOpponent",
-    kwargs={"mode": 0, "weak_opponent": False},
-  )
-  REGISTERED_ENVS = True
+    register(
+        id="Hockey-v0",
+        entry_point="hockey.hockey_env:HockeyEnv",
+        kwargs={"mode": 0},
+    )
+    register(
+        id="Hockey-One-v0",
+        entry_point="hockey.hockey_env:HockeyEnv_BasicOpponent",
+        kwargs={"mode": 0, "weak_opponent": False},
+    )
+    REGISTERED_ENVS = True
 except Exception as e:
-  logging.error("Error registering environments in hockey:", e)
+    logging.error("Error registering environments in hockey:", e)
