72b82def57138595cefa9e7b8b7f77b5902d4b53
Niclas Lietzow <niclas.lietzow@outlook.com>
2025-02-08 06:34:24 +0100

    update hockey env

--- hockey/register.py
+++ hockey/register.py
@@ -2,13 +2,19 @@ from gymnasium.envs.registration import register
 
 register(
     id="Hockey-v0",
-    entry_point="hockey.hockey_env:HockeyEnv",
-    kwargs={"mode": 0},
+    entry_point="hockey.hockey_env_v2:HockeyEnv",
 )
 register(
-    id="Hockey-One-v0",
-    entry_point="hockey.hockey_env:HockeyEnvWithOpponent",
-    kwargs={"mode": 0},
+    id="Hockey-One-v0-BasicOpponent",
+    entry_point="hockey.hockey_env_v2:HockeyEnvBasicOpponent",
+)
+register(
+    id="Hockey-One-v0-AIOpponent",
+    entry_point="hockey.hockey_env_v2:HockeyEnvAIOpponent",
 )
 
-REGISTERED_ENVS = ["Hockey-v0", "Hockey-One-v0"]
+REGISTERED_ENVS = [
+    "Hockey-v0",
+    "Hockey-One-v0-BasicOpponent",
+    "Hockey-One-v0-AIOpponent",
+]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="hockey",
-    version="2.32",
+    version="2.33",
     description="Simple Hockey Environments",
     url="https://github.com/martius-lab",
     author="Georg Martius, Uni Tuebingen, Autonomous Learning",
